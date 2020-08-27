129a8440e9f5b009d081679793bd5d9bda282bc9
RHellgren <rhellgren@icloud.com>
2020-08-27 17:57:15 +0100

    Start new project

---
+++ BGG/BGG/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  BGG
+//
+//  Created by Robin Hellgren on 27/08/2020.
+//  Copyright © 2020 Robin Hellgren. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ BGG/BGG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BGG
+//
+//  Created by Robin Hellgren on 27/08/2020.
+//  Copyright © 2020 Robin Hellgren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
